de6d8f6122f243a653c410cc5ac8c709716561ad
quan-code <54423805+quan-code@users.noreply.github.com>
2020-04-29 14:19:45 +0800

    Update Model.java

--- app/src/main/java/com/example/a5/Model.java
+++ app/src/main/java/com/example/a5/Model.java
@@ -1,5 +1,5 @@
-package com.example.a5;
-
+package com.example.a5; 
+// implenment a model that will be used in two class
 public class Model {
     public String mDate, mItem;
     public double mPrice;
